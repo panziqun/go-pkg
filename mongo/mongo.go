e4e8499dc584c30ccc43b83bc8bcc8a9f39ce67f
laughmaker <674081011@qq.com>
2019-03-03 20:12:32 +0800

    修改mongo传参类型

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/laughmaker/go-pkg/conf"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,7 +23,7 @@ func Setup() (err error) {
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
-		fmt.Println("new client err:%v", err)
+		fmt.Printf("new client err:%v", err)
 		return err
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
@@ -32,13 +31,13 @@ func Setup() (err error) {
 	err = client.Connect(ctx)
 	Database = client.Database(conf.MongodbConf.Database)
 	if err != nil {
-		fmt.Println("client connect err:%v", err)
+		fmt.Printf("client connect err:%v", err)
 		return err
 	}
 	return err
 }
 
-func InsertOne(name string, data bson.M) (id interface{}, err error) {
+func InsertOne(name string, data interface{}) (id interface{}, err error) {
 	res, err := Database.Collection(name).InsertOne(context.Background(), data)
 	if err != nil {
 		return nil, err
@@ -46,7 +45,7 @@ func InsertOne(name string, data bson.M) (id interface{}, err error) {
 	return res.InsertedID, nil
 }
 
-func One(name string, filter bson.D) (model interface{}, err error) {
+func One(name string, filter interface{}) (model interface{}, err error) {
 	err = Database.Collection(name).FindOne(context.Background(), filter).Decode(&model)
 	if err != nil {
 		return nil, err
@@ -54,7 +53,7 @@ func One(name string, filter bson.D) (model interface{}, err error) {
 	return model, nil
 }
 
-func All(name string, filter bson.D) (list []interface{}, err error) {
+func All(name string, filter interface{}) (list []interface{}, err error) {
 	cur, err := Database.Collection(name).Find(context.Background(), filter)
 	if err != nil {
 		log.Fatal(err)
